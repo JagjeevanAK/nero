9f1eac3ea05c50306c5bacc1ad2e43831a16f961
Jagjeevan <83103469+JagjeevanAK@users.noreply.github.com>
2025-04-18 17:06:27 +0530

    solved minor errors
    Signed-off-by: Jagjeevan <83103469+JagjeevanAK@users.noreply.github.com>

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,16 +4,17 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/',  // Explicitly set base path
   plugins: [react(), tailwindcss(),],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-    server: {
-      proxy: {
-        '/api': {
-          target: 'http://localhost:3000',
-          changeOrigin: true,
-        },
-      }
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+      },
     }
+  }
 })
