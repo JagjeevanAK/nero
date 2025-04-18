70dd1d50b38d43619fa860011abef79c81126b29
Yashrajsalunkhe <yashrajsalunkhe6@gmail.com>
2025-04-18 10:53:21 +0530

    changing fee to 80
    Signed-off-by: Yashrajsalunkhe <yashrajsalunkhe6@gmail.com>

--- src/data/events.ts
+++ src/data/events.ts
@@ -5,7 +5,7 @@ export const events = [
     id: 1,
     title: "Group Discussion (GD)",
     icon: MessageCircle,
-    fee: 70,
+    fee: 80,
     description: "Showcase your communication skills, analytical thinking, and leadership in a moderated group setting.",
     requiresTeam: false
   },
@@ -13,7 +13,7 @@ export const events = [
     id: 2,
     title: "Technical Marathon",
     icon: Code2,
-    fee: 70,
+    fee: 80,
     description: "Test your technical knowledge and problem-solving skills in this coding and technical challenge.",
     requiresTeam: true
   },
@@ -21,7 +21,7 @@ export const events = [
     id: 3,
     title: "Dock The Flag",
     icon: Linux,
-    fee: 70,
+    fee: 80,
     description: "Put your Linux knowledge to the test in this fast-paced quiz competition about the open-source OS.",
     requiresTeam: false
   },
