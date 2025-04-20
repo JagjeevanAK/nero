69e817b6fd6a7f5ad947f786288454a7b8998835
Jagjeevan <83103469+JagjeevanAK@users.noreply.github.com>
2025-04-20 22:44:16 +0530

    added some design changes
    Signed-off-by: Jagjeevan <83103469+JagjeevanAK@users.noreply.github.com>

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,15 @@
+import { dirname, resolve } from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { fileURLToPath } from 'url'
 
-// https://vite.dev/config/
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
 export default defineConfig({
+  // Load .env from project root (one level above frontend)
+  envDir: resolve(__dirname, '../'),
   base: '/',  // Explicitly set base path
   plugins: [react(), tailwindcss(),],
   optimizeDeps: {
