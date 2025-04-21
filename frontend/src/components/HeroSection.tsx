974463bbd833624ed9fab35b00b8404a7b3c31d2
Rakesh <rakeshyadavry087@gmail.com>
2025-04-21 19:39:42 +0530

    caution

--- frontend/src/components/HeroSection.tsx
+++ frontend/src/components/HeroSection.tsx
@@ -4,7 +4,7 @@ import { motion } from "framer-motion";
 const HeroSection: React.FC = () => (
   <>
     {/* Caution Message */}
-    <div className="fixed w-140 top-4 right-4 bg-red-600 text-white text-sm font-bold px-4 py-2 rounded-lg shadow-lg z-50">
+    <div className="fixed max-w-140 top-4 mx-10 bg-red-600 text-white text-sm font-bold px-4 py-2 rounded-lg shadow-lg z-50">
       ⚠️ Caution: Don't register yet, the website is under maintenance!
       Registrations start soon!!
     </div>
